e68c97716390d9780f96ee520ca960a09c9c804e
IoanValentinCernovschi <61148592+IoanValentinCernovschi@users.noreply.github.com>
2020-03-26 20:37:13 +0200

    Add files via upload

---
+++ User.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public abstract class User {
+    protected String name;
+    protected int age;
+    protected String address;
+    protected String email;
+    protected String telephone;
+
+    abstract boolean login();
+    abstract boolean sendMessage(User user, String message);
+    abstract Doctor searchDoctor(String name);
+    abstract <T> List<Doctor> sortDoctors(List<Doctors> doctorsList, Comparator<T> comparator);
+    abstract Specialization searchSpecialization(String name);
+    abstract List<Doctor> filterDoctors(String criteria);
+}
